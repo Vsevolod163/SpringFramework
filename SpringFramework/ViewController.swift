8b7be7d9ccd9cca1f5908746964c2fde54927540
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-01 22:06:26 +0300

    SpringAnimation framework added

--- SpringFramework/ViewController.swift
+++ SpringFramework/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 final class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
+    
+    private var animationStarted = false
 
     @IBAction func runCoreAnimation(_ sender: UIButton) {
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat]) { [unowned self] in
+                if !animationStarted {
+                    coreAnimationView.frame.origin.x -= 40
+                    animationStarted.toggle()
+                }
+            }
+
         sender.pulsate()
     }
     
