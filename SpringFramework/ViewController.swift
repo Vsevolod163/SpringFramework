20fb838cd431db2e1eabbbd8820b5e087de5d143
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-01 21:17:07 +0300

    pulsate func and animation added

--- SpringFramework/ViewController.swift
+++ SpringFramework/ViewController.swift
@@ -12,7 +12,7 @@ final class ViewController: UIViewController {
     @IBOutlet var coreAnimationView: UIView!
 
     @IBAction func runCoreAnimation(_ sender: UIButton) {
-        
+        sender.pulsate()
     }
     
 }
