25c8033c25de404b47a4b5a3f1fea64e7c93848f
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-01 22:29:34 +0300

    runSpringAnimation func added

--- SpringFramework/ViewController.swift
+++ SpringFramework/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import SpringAnimation
 
 final class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet var springAnimationView: SpringView!
     
     private var animationStarted = false
 
@@ -27,5 +29,15 @@ final class ViewController: UIViewController {
         sender.pulsate()
     }
     
+    @IBAction func runSpringButton(_ sender: SpringButton) {
+        springAnimationView.animation = "shake"
+        springAnimationView.curve = "easeInOut"
+        springAnimationView.force = 2
+        springAnimationView.animate()
+        
+        sender.animation = "pop"
+        sender.duration = 2
+        sender.animate()
+    }
 }
 
